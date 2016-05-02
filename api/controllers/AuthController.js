cad792b46b5ba04038d39207d3c9bff531fefdbb
Tyler Leonhardt <tylerl0706@gmail.com>
2016-05-02 19:46:33 -0400

    styles and auth

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -4,6 +4,28 @@
  */
 
 var AuthController = {
+  // login: function(req, res) {
+  //   LDAPService.authenticate(
+  //     req, (sails.config.auth || {}).ldap,
+  //     function(err, user) {
+
+  //       if (err) {
+  //         return res.send(err.code || 401, err.message || 'Incorrect credentials');
+  //       }
+
+  //       if (!user) {
+  //         // If there is no error passed then we should have a user object
+  //         return res.serverError('Could not retrieve user');
+  //       }
+
+  //       return res.json({
+  //         user: user.sAMAccountName,
+  //         token: AuthToken.issueToken({
+  //           sub: user.sAMAccountName
+  //         })
+  //       });
+  //     });
+  // }
   login: function(req, res) {
     AuthService.authenticate(
       req,
